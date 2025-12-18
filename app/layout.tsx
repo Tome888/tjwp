c77722f27e3d7b5225258fb2c01cc23c87cd7004
Tome888 <tomejeftimov8@gmail.com>
2025-12-18 20:36:23 +0100

    change logos

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,19 +18,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/tj-light.svg",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/tj-dark.svg",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/tj-dark.svg",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/tj-light.svg",
   },
 };
 
